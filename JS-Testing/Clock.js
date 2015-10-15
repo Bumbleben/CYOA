2aaefcb107c510f30a426c39001742c9d422768e
Bumbleben <bencatechi@me.com>
2015-10-14 17:43:01 -0700

    :warning:

--- JS-Testing/Clock.js
+++ JS-Testing/Clock.js
@@ -1,5 +1,10 @@
 var userPass = {ben175:"bensam", happy:"yolo", sexy:"shmexy"}
 var checkPass = function() {
-	var user = prompt("Put in your username.","Username")
-	
+	var user = prompt("Put in your username.","Username");
+	var pass = prompt("Put in your password.", "Password");
+	if (userPass[user] === pass) {
+		alert("We're in.")
+	} else {
+		alert("We're still trying.")
+	}
 }
